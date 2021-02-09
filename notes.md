1da1df6599b7e8cfd2bb98e531c86bf3d0e17186
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2021-02-09 06:06:53 -0600

    Basic Setup
    - setup the most obvious variables and event listeners
    - IMPORTANT: in my index.html file, i changed the script from the production one to the development one
    - i will change it back to the minified and transpiled one when app is done and ready to be deployed
    - il y a peu de logique, s'il y en a

---
+++ notes.md
@@ -0,0 +1,4 @@
+## Notes
+
+* Setup variables and event listeners
+* There is very little logic in this branch (if any)
--- src/js/script.js
+++ src/js/script.js
@@ -4,10 +4,30 @@ const DOM = (function () {
   const elements = {
     inputGuess: document.querySelector(".guess"),
     btnCheck: document.querySelector(".check"),
+    correctNumber: document.querySelector(".number"),
+    message: document.querySelector(".message"),
   };
   return elements;
 })();
 
-DOM.btnCheck.addEventListener("click", (e) => {
-  console.log(e);
+console.log(DOM);
+
+DOM.btnCheck.addEventListener("click", () => {
+  const inputValue = DOM.inputGuess.value;
+
+  if (inputValue === "") {
+    console.log("Nothing was typed. Réessayer à nouveau");
+  } else {
+    const inputValueNum = Number(Math.trunc(inputValue));
+
+    console.log(inputValueNum);
+    console.log(typeof inputValueNum);
+
+    DOM.message.textContent = `The number ${inputValueNum} was submitted`;
+  }
+});
+
+window.addEventListener("load", () => {
+  console.log("Tout a été reconfiguré");
+  console.log("C'est ici ou le chiffre correque sera reinitialisé");
 });
