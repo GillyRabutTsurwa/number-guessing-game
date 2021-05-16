32616c91a99c9830e8038379773ebc80fa3a9f12
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2021-05-15 22:29:25 -0400

    Refactored Code - Part 1 - refactored code from the previous feature branch (which is now deleted due to git-flow workflow) - my javascript file looks much cleaner

--- notes.md
+++ notes.md
@@ -1,8 +1,12 @@
 ## Notes
 
-* Added the logic of the game
-  * If you press the check button with no number, it will prompt to actually input a number
-  * I implemented the code for guessing, if they correctly guess the number, and if the wrongly guess the number.
-  * Enough wrong guesses will lead to game over
-  * There's a place in the code where I could have used a simple if statement. But instead I used a switch statement to challenge myself.
-  * All looks good. We move. 
+- Here, I am refactoring the code I made from the last commit, which was the feature branch *game-logic-part-1*
+- The code is very messy and needs cleaning up and modularising
+- okay after some time, the code looks much cleaner
+- i am taking this code out of my javascript file and putting it here. it refers to the switch statement in the script.js file
+  <!-- 
+  - i could use a simple if statement, but I'm a rogue, so I'm using switch instead
+  - OK I see. switch is weird. it takes a value, usually variable and checks if that variable is === to the value switch takes in.
+  - i'm using switch in a weird way. this post explains nicely how to use it in my intended use case. 
+  mdn docs are nice too, mais pas ce que je cherchais: http://www.dynamicdrive.com/forums/showthread.php?32038-Convert-from-If-If-Else-to-Switch-Statement-how-is-it-done 
+  -->
--- src/js/script.js
+++ src/js/script.js
@@ -15,16 +15,29 @@ const DOM = (function () {
   return elements;
 })();
 
+
+// ==================================== FUNCTIONS ====================================
+const gameDone = (gameMsg, bgColour, lineAndFontColour) => {
+  DOM.message.textContent = gameMsg;
+  DOM.body.style.backgroundColor = bgColour;
+  DOM.inputGuess.disabled = true;
+  DOM.btnCheck.disabled = true;
+  DOM.btnCheck.style.cursor = "not-allowed";
+  DOM.header.style.borderBottomColor = lineAndFontColour;
+  DOM.correctNumber.style.border = `5px solid ${lineAndFontColour}`;
+  DOM.inputGuess.style.borderColor = lineAndFontColour;
+  [DOM.title, DOM.inputGuess, ...DOM.allParagraphs].forEach((currentElement) => {
+    currentElement.style.color = lineAndFontColour;
+  });
+  gameOver = true;
+}
+
+
+
 let gameOver = false;
-
-
-// NOTE: secret number that will be randomised upon each game
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
-// NOTE: player's starting score
 let score = 20;
 
-console.log(DOM);
 
 if (!gameOver) {
   DOM.btnCheck.addEventListener("click", () => {
@@ -35,62 +48,36 @@ if (!gameOver) {
     } else {
       const inputValueNum = Number(Math.trunc(inputValue));
   
-      console.log(inputValueNum);
-      console.log(typeof inputValueNum);
-  
-      // I could use a simple if statement, but I'm a rogue, so I'm using switch instead
-      // we using switch instead.
-      // OK I see. switch is weird. it takes a value, usually variable and checks if that variable is === to the value switch takes in.
-      // I'm using switch in a weird way. this post explains nicely how to use it in my intended use case. mdn docs are nice too, mais pas ce que je cherchais: http://www.dynamicdrive.com/forums/showthread.php?32038-Convert-from-If-If-Else-to-Switch-Statement-how-is-it-done
-  
       if (score > 1) {
         switch (true) {
           case inputValueNum < secretNumber:
             DOM.message.textContent = "📉 Too low";
             score--;
-            console.log("Je suis moins que le chiffre secret");
             break;
   
           case inputValueNum > secretNumber:
             DOM.message.textContent = "📈 Too High";
-            console.log("I am more than the secret number");
             score--;
             break;
   
           case inputValueNum === secretNumber:
-            console.log("Voila, tu mas trouve");
-            DOM.message.textContent = "👏🏿👏🏿👏🏿 Well Done";
             DOM.correctNumber.textContent = secretNumber;
-            DOM.body.style.backgroundColor = "#60b347";
-            DOM.inputGuess.disabled = true;
-            DOM.btnCheck.disabled = true;
-            DOM.btnCheck.style.cursor = "not-allowed";
-            DOM.header.style.borderBottomColor = "#222";
-            DOM.correctNumber.style.border = "5px solid #222";
-            DOM.inputGuess.style.borderColor = "#222";
-            [DOM.title, DOM.inputGuess, ...DOM.allParagraphs].forEach((currentElement) => {
-              currentElement.style.color = "#222";
-            })
-            gameOver = true;
+            gameDone("👏🏿👏🏿👏🏿 Well Done", "#60b347", "#222");
             break;
-  
+   
           default:
             break;
         }
       } else {
         score = 0;
-        DOM.message.textContent = `Game Over. You somehow reached to ${score} points`;
-        DOM.inputGuess.disabled = true;
-        gameOver = true;
-        DOM.btnCheck.disabled = true;
-        DOM.btnCheck.style.cursor = "not-allowed";
+        const gameOverMsg = `Game Over. The secret number that escaped you was ${secretNumber}`;
+        gameDone(gameOverMsg, "#b347af", "#222");
       }
   
       DOM.currentScore.textContent = score;
     }
   });
 }
-
 
 window.addEventListener("load", () => {
   gameOver = false;
